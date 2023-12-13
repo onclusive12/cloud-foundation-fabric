51f173a577c942e382fadb132481e32b34264a79
ONC-GCP-POC <153643650+onclusive12@users.noreply.github.com>
2023-12-13 12:32:14 +0530

    Update providers.tf

--- vpc/providers.tf
+++ vpc/providers.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.7.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "fast-fabric-bucket"
+    prefix = "dev"
+  }
+
+}
+provider "google" {
+  project     = "onclusive-poc"
+  region      = "asia-south1"
+}
+
 
