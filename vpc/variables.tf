6bd2ab9eaca4a140b31d458ccd027dabca4966a7
ONC-GCP-POC <153643650+onclusive12@users.noreply.github.com>
2023-12-13 16:50:34 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,12 +1,40 @@
 variable "project_id" {
   description = "The ID of the project where this VPC will be created."
   type        = string
+  default     = "onclusive-poc"
 }
 
 variable "name" {
   description = "The name of the network being created."
   type        = string
+  default     = "my-network"
 }
 
+variable "ip_cidr_range" {
+  description = "CIDR blocks for subnets in the VPC"
+  type        = list(string)
+  default     = "10.0.0.0/24"
+}
 
+variable "region" {
+  description = "region for subnets"
+  type        = string
+  default     = "europe-west1"
+}
 
+variable "subnets" {
+  type = object({
+    ip_cidr_range      = string
+    name               = string
+    region             = string
+    
+  })
+  description = "List of subnets"
+default = [
+    
+      ip_cidr_range = "10.0.0.0/24"
+      name          = "production"
+      region        = "europe-west1"
+      
+  ]
+}
