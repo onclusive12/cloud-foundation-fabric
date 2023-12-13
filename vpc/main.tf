4343d5ee11df3a3c0fa1901feee3fe35679dd0b8
ONC-GCP-POC <153643650+onclusive12@users.noreply.github.com>
2023-12-13 17:14:39 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,6 +2,12 @@ module "vpc" {
   source     = "../modules/net-vpc"
   project_id = var.project_id
   name       = var.name
-  subnets = var.subnets
+  subnets = [
+    {
+      ip_cidr_range = var.subnets["ip_cidr_range"]
+      name          = var.subnets["name"]
+      region        = var.subnets["region"]
+    }
+  ]
 }
 # tftest modules=1 resources=5 inventory=simple.yaml e2e
