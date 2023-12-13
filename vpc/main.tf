15edb37efb2164d11d0428ff4ff7a2fba96e2e55
ONC-GCP-POC <153643650+onclusive12@users.noreply.github.com>
2023-12-13 12:30:17 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,1 +1,22 @@
-
+module "vpc" {
+  source     = "./fabric/modules/net-vpc"
+  project_id = var.project_id
+  name       = "my-network"
+  subnets = [
+    {
+      ip_cidr_range = "10.0.0.0/24"
+      name          = "production"
+      region        = "europe-west1"
+      secondary_ip_ranges = {
+        pods     = "172.16.0.0/20"
+        services = "192.168.0.0/24"
+      }
+    },
+    {
+      ip_cidr_range = "10.0.16.0/24"
+      name          = "production"
+      region        = "europe-west2"
+    }
+  ]
+}
+# tftest modules=1 resources=5 inventory=simple.yaml e2e
