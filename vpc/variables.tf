1366f1e4e01f8af619ece722a277ee329f49dafc
ONC-GCP-POC <153643650+onclusive12@users.noreply.github.com>
2023-12-13 16:24:55 +0530

    Create variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "The ID of the project where this VPC will be created."
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the network being created."
+  type        = string
+}
+
+
+
